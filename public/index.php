cd18c6430a2b2fb5ba009f74202a7971fe99f1ea
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-10-04 06:04:53 +0700

    add: pretty error page and dump

--- public/index.php
+++ public/index.php
@@ -6,10 +6,17 @@ use Dotenv\Exception\InvalidPathException;
 
 require '../vendor/autoload.php';
 
+// Load dotenv configurations
 try {
     $env = Dotenv::createImmutable(__DIR__ . '/..');
     $env->load();
 } catch(InvalidPathException $e) {}
 
+// Initialize pretty error page
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+// Run Application
 $app = Application::register();
 $app->run();
