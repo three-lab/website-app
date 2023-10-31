ed85fa53e9a952bda23e9a1314a678b85510a151
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-10-31 08:31:16 +0700

    chore(validation): add alias for each field

--- app/Requests/EmployeeRequest.php
+++ app/Requests/EmployeeRequest.php
@@ -9,20 +9,22 @@ class EmployeeRequest extends Request
     protected function rules(): array
     {
         $rules = [
-            'nik' => 'required',
-            'fullname' => 'required',
-            'birthdate' => 'required',
-            'email' => 'required|email',
-            'username' => 'required',
-            'gender' => 'required',
-            'images' => 'required|array',
+            'nik:NIK' => 'required',
+            'fullname:Nama Lengkap' => 'required',
+            'birthdate:Tgl. Lahir' => 'required',
+            'email:Email' => 'required|email',
+            'username:Username' => 'required',
+            'gender:Jenis Kelamin' => 'required',
+            'images:Gambar' => 'required|array',
         ];
 
         if($this->isMethod('POST')) {
             $rules = array_merge(
                 $rules,
                 [
-                    'images.*' => 'required|uploaded_file|mimes:jpg,png,jpeg',
+                    'images.front:Foto Depan' => 'required|uploaded_file|mimes:jpg,png,jpeg',
+                    'images.left:Foto Kiri' => 'required|uploaded_file|mimes:jpg,png,jpeg',
+                    'images.right:Foto Kanan' => 'required|uploaded_file|mimes:jpg,png,jpeg',
                     'password' => 'required',
                 ]
             );
