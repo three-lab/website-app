dc7670938f1a2c44d6e0cfdd48a04530dc6858b1
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-12-06 12:46:03 +0700

    chore: fix auto excuse status update

--- app/Commands/ManageAttendance.php
+++ app/Commands/ManageAttendance.php
@@ -3,17 +3,21 @@
 namespace App\Commands;
 
 use Ahc\Cli\Input\Command;
+use App\Models\Excuse;
 use App\Repos\AttendanceRepo;
+use App\Repos\ExcuseRepo;
 
 class ManageAttendance extends Command
 {
     private AttendanceRepo $attendanceRepo;
+    private ExcuseRepo $excuseRepo;
 
     public function __construct()
     {
         parent::__construct('manage-attendance', 'Manage attendance data');
 
         $this->attendanceRepo = new AttendanceRepo;
+        $this->excuseRepo = new ExcuseRepo;
     }
 
     public function execute()
@@ -21,5 +25,6 @@ class ManageAttendance extends Command
         $this->attendanceRepo->insertDaily(date('N'));
         $this->attendanceRepo->makeAbsences(date('Y-m-d'));
         $this->attendanceRepo->endUncompleteAttendance();
+        $this->excuseRepo->updateExuceStatus();
     }
 }
--- app/Repos/ExcuseRepo.php
+++ app/Repos/ExcuseRepo.php
@@ -5,6 +5,7 @@ namespace App\Repos;
 use App\Models\Employee;
 use App\Models\Excuse;
 use Cake\Chronos\Chronos;
+use PDO;
 use System\Support\UploadedFile;
 
 class ExcuseRepo
@@ -30,6 +31,20 @@ class ExcuseRepo
             'date_start' => date('Y-m-d'),
             'date_end' => $endDate->format('Y-m-d'),
             'file' => $name,
+        ]);
+
+        $this->updateExuceStatus();
+    }
+
+    public function updateExuceStatus()
+    {
+        $conn = $this->excuse->conn();
+        $stmt = $conn->prepare("UPDATE attendances SET status = 'excused' WHERE employee_id IN(
+            SELECT employee_id FROM excuses WHERE date_start >= :date AND date_end <= :date GROUP BY(employee_id)
+        ) AND date = :date");
+
+        $stmt->execute([
+            'date' => date('Y-m-d'),
         ]);
     }
 }
